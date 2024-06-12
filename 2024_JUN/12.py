5f067081141e4357567bf0dc2d0aba67a6445980
sajad kh <sajadph79@gmail.com>
2024-06-13 03:02:09 +0330

    problem 2024-JUN-12

---
+++ 2024_JUN/12.py
@@ -0,0 +1,39 @@
+"""
+   ______                        _           __     ______    
+  / ____/___       _________ _  (_)___ _____/ /    / ____/___ 
+ / / __/ __ \     / ___/ __ `/ / / __ `/ __  /    / / __/ __ \ 
+/ /_/ / /_/ /    (__  ) /_/ / / / /_/ / /_/ /    / /_/ / /_/ /
+\____/\____/    /____/\__,_/_/ /\__,_/\__,_/     \____/\____/ 
+                          /___/                               
+
+Daily coding problem (Algorithm, DB and etc)
+
+Date: 2024-JUN-112
+
+app: leetcode
+problem: https://leetcode.com/problems/3sum/description/
+"""
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        nums.sort()
+        k = len(nums) - 1
+        dd = []
+        for i in range(len(nums)):
+            if nums[i] in dd:
+                continue
+            dd.append(nums[i])
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0:
+                    ans.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif sum < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return ans
