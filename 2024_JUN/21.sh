3e00f44140c7ac95fe545bf8da7bb157dba3c6ac
sajad <sajadph79@gmail.com>
2024-06-21 23:35:35 +0330

    problem 2024-JUN-21

---
+++ 2024_JUN/21.sh
@@ -0,0 +1,28 @@
+"""
+   ______                        _           __     ______    
+  / ____/___       _________ _  (_)___ _____/ /    / ____/___ 
+ / / __/ __ \     / ___/ __ `/ / / __ `/ __  /    / / __/ __ \ 
+/ /_/ / /_/ /    (__  ) /_/ / / / /_/ / /_/ /    / /_/ / /_/ /
+\____/\____/    /____/\__,_/_/ /\__,_/\__,_/     \____/\____/ 
+                          /___/                               
+
+Daily coding problem (Algorithm, DB and etc)
+
+Date: 2024-JUN-21
+
+app: leetcode
+problem: https://leetcode.com/problems/valid-phone-numbers/description/
+"""
+
+# Define the input file
+INFILE=file.txt
+
+# Read the input file line by line
+while read -r LINE
+do
+    if [[ "$LINE" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]; then
+        echo "$LINE"
+    elif [[ "$LINE" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]]; then
+        echo "$LINE"
+    fi
+done < "$INFILE"
