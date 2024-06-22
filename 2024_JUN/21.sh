e4e7050dc054c801ef00f9174533749c13e1d79f
sajad <sajadph79@gmail.com>
2024-06-23 00:52:38 +0330

    edit comment sh file

--- 2024_JUN/21.sh
+++ 2024_JUN/21.sh
@@ -1,18 +1,17 @@
-"""
-   ______                        _           __     ______    
-  / ____/___       _________ _  (_)___ _____/ /    / ____/___ 
- / / __/ __ \     / ___/ __ `/ / / __ `/ __  /    / / __/ __ \ 
-/ /_/ / /_/ /    (__  ) /_/ / / / /_/ / /_/ /    / /_/ / /_/ /
-\____/\____/    /____/\__,_/_/ /\__,_/\__,_/     \____/\____/ 
-                          /___/                               
+#    ______                        _           __     ______    
+#   / ____/___       _________ _  (_)___ _____/ /    / ____/___ 
+#  / / __/ __ \     / ___/ __ `/ / / __ `/ __  /    / / __/ __ \ 
+# / /_/ / /_/ /    (__  ) /_/ / / / /_/ / /_/ /    / /_/ / /_/ /
+# \____/\____/    /____/\__,_/_/ /\__,_/\__,_/     \____/\____/ 
+#                           /___/                               
 
-Daily coding problem (Algorithm, DB and etc)
+# Daily coding problem (Algorithm, DB and etc)
 
-Date: 2024-JUN-21
+# Date: 2024-JUN-21
 
-app: leetcode
-problem: https://leetcode.com/problems/valid-phone-numbers/description/
-"""
+# app: leetcode
+# problem: https://leetcode.com/problems/valid-phone-numbers/description/
+
 
 # Define the input file
 INFILE=file.txt
